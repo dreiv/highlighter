c0055810ad1a9c6957084042fe79533d41659556
dreiv <dreivoicu@gmail.com>
2018-06-07 16:43:58 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      
         {data.map((el, idx) => <div key={idx}>{el.transcript}</div>)}
       </div>
     );
