8c891cde49616fa96043f0dca9d9769263b818dc
Petrut <PetrutP@haufe.com>
2018-06-07 17:48:38 +0300

    Added string replace and search as prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import data from './data.json'
 import './App.css';
+import Highlight from "./components/Highlight"
 
 class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-      
-        {data.map((el, idx) => <div key={idx}>{el.transcript}</div>)}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Highlight>
+					{data.map((el, idx) => <div key={idx}>{el.transcript}</div>)}
+				</Highlight>
+			</div>
+		);
+	}
 }
 
 export default App;
