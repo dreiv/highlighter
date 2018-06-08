0ed02e14cfdc61b679a62c7b4ed7f7f82c34161b
Petrut <PetrutP@haufe.com>
2018-06-08 10:39:50 +0300

    Added functionality to highlight text from props

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -4,9 +4,21 @@ import { escapeRegExpFn, isScalar } from '../utils/utils'
 class Highlight extends Component {
 
 	render() {
-		const fn = (str) => {
+		const fn = (text) => {
+
+			const allChunks = this.constructAllChunks(this.computeHighlightedChunks(text), text.length)
+
 			return (
-				<h1>{str.replace(this.props.searchWord, ">>>>>replaceed<<<<<<")}</h1>
+				<React.Fragment>
+					{allChunks.map((chunk, index) => {
+						const chunkText = text.substr(chunk.start, chunk.end - chunk.start)
+
+						if (chunk.highlight) {
+							return <mark>{chunkText}</mark>
+						}
+						return <React.Fragment>{chunkText}</React.Fragment>
+					})}
+				</React.Fragment>
 			)
 		}
 
@@ -27,6 +39,38 @@ class Highlight extends Component {
 		return <React.Fragment>
 			{recursiveCloneChildren(this.props.children, fn)}
 		</React.Fragment>
+	}
+
+	computeHighlightedChunks(text) {
+
+		const searchWord = this.props.searchWord;
+		const regExp = new RegExp(searchWord, 'gi')
+
+		const chunks = []
+		let match
+		while ((match = regExp.exec(text)) !== null) {
+			const start = match.index
+			const end = regExp.lastIndex
+			chunks.push({ start, end, highlight: true })
+		}
+
+		return chunks
+	}
+
+	constructAllChunks(chunks, totalLength) {
+
+		const allChunks = []
+
+		let lastFoundIndex = 0
+		chunks.forEach((chunk) => {
+			allChunks.push({ start: lastFoundIndex, end: chunk.start, highlight: false })
+			allChunks.push({ start: chunk.start, end: chunk.end, highlight: true })
+			lastFoundIndex = chunk.end
+		})
+
+		allChunks.push({ start: lastFoundIndex, end: totalLength, highlight: false })
+
+		return allChunks
 	}
 }
 
